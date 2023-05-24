aa363b9829967c10387e4bcf33224fe8aa09b541
sharmaniraj009 <sharmaniraj009@gmail.com>
2023-05-24 19:04:23 +0530

    decorated answers by the news client

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ load_dotenv(".env")
 get_articles_api = NewsCatcherApiClient(x_api_key=os.getenv("api"))
 
 results = get_articles_api.get_latest_headlines(lang='en', countries='in',page_size=10)
-# print(results['articles']['title'])
-# print(results['articles']['published_date'])
-# print(results['articles']['link'])
 
 def getHeadline(message) -> str:
     i_message = message.lower()
--- news_test.py
+++ news_test.py
@@ -6,15 +6,14 @@ import os
 load_dotenv(".env")
 get_articles_api = NewsCatcherApiClient(x_api_key=os.getenv("api"))
 
-results = get_articles_api.get_latest_headlines(lang='en', countries='in',page_size=10)
+results = get_articles_api.get_latest_headlines(lang='en', countries='us',page_size=10)
 # print(results['articles']['title'])
 # print(results['articles']['published_date'])
 # print(results['articles']['link'])
 
 
-for i in range(1,10):
-    print(f"{i}. {results['articles'][i]['title']} a quick summary of the headline : {results['articles'][i]['summary']} ")
-    
-# print("the bot is under development. more functionalities will be added in some time. thank you for showing patience")
+for i in range(1,2):
+    print(f"{i}. News section : {results['articles'][i]['topic']} {results['articles'][i]['title']} a quick summary of the headline : {results['articles'][i]['summary']}. news is from {results['articles'][i]['country']} ")
 
-    # return ("enter a valid command")
+# k = results['articles'][0]
+# print(k.keys())
